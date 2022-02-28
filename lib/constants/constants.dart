f72aac03921db3dde5df7eb66dac445379d73631
Pankaj Tungaria PK <36400901+geek-pk@users.noreply.github.com>
2022-02-28 12:52:55 +0530

    Update constants.dart

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,6 +1,5 @@
-// const apiKey = 'f1e7882f59db4b9ba2d9b9a6698811f5';7825c81e964d463494a771fcbcc32ed9
 const baseUrl = 'https://newsapi.org/v2/';
-const apiKey = '7825c81e964d463494a771fcbcc32ed9';
+const apiKey = 'YOUR API KEY';
 const appName = 'News App';
 
 const timeFrom = '2022-02-18';
